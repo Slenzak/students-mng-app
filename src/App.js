b0255bd209264d5d58240bbf4d7f52e3f79502a7
Slenzak <kubolek2007.1997@wp.pl>
2025-04-25 11:19:50 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
 import { BrowserRouter,Route,Routes } from "react-router-dom";
+import MainPage from "./pages/mainpage";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />}/>
-        <Route path="/" element={<MainPage />}/>
-        <Route path="/" element={<MainPage />}/>
-        <Route path="/" element={<MainPage />}/>
+        
       </Routes>
     </BrowserRouter>
   );
