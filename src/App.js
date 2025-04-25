9c610123259d9dc10789c36361fc00a535fee2cc
Slenzak <kubolek2007.1997@wp.pl>
2025-04-25 12:24:34 +0200

    quickfix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import { BrowserRouter,Route,Routes } from "react-router-dom";
 import MainPage from "./pages/mainpage";
 import ClassDetailsPage from "./pages/classdetailpage";
 import AddStudentPage from "./pages/addstudentpage";
+import { initData } from "./utility/storage";
 function App() {
+    useEffect(() =>{
+      initData();
+    },[]);
   return (
     <BrowserRouter>
       <Routes>
